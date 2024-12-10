f03aabb6b737591f1998fbc2b18305ffdcf1b98a
Andrey Sarantsev <50476640+asarantsev@users.noreply.github.com>
2024-12-10 11:12:02 -0800

    Add files via upload

--- standard-curve-simulation.py
+++ standard-curve-simulation.py
@@ -2,10 +2,28 @@ import numpy
 from matplotlib import pyplot as plt
 
 K = 100
-Xaxis = [numpy.log(k) for k in range(1, K+1)]
+Xaxis = numpy.log(numpy.array(range(1, K+1)))
 NSIMS = 3
 for sim in range(NSIMS):
     interJumpTimes = numpy.random.exponential(1, K)
-    jumpTimes = -numpy.log(numpy.cumsum(interJumpTimes))
-    plt.plot(Xaxis, jumpTimes)
+    jumpTimes = numpy.log(numpy.cumsum(interJumpTimes))
+    plt.plot(Xaxis, -jumpTimes)
+plt.plot(Xaxis, -Xaxis, 'black', linestyle = 'dashed')
+plt.xlabel('log ranks')
+plt.ylabel('log weights')
+plt.title('Upper Left End')
 plt.show()
+
+N = 500
+Xaxis = numpy.log(numpy.array(range(N-K, N)))
+for sim in range(NSIMS):
+    interJumpTimes = numpy.random.exponential(1, K)
+    jumpTimes = numpy.log(numpy.cumsum(interJumpTimes))
+    plt.plot(Xaxis[::-1], jumpTimes)
+Function = numpy.array([numpy.log(N-numpy.exp(k)) for k in Xaxis])
+plt.plot(Xaxis, Function, 'black', linestyle = 'dashed')
+plt.xlabel('log ranks')
+plt.ylabel('log weights')
+plt.title('Lower Right End')
+plt.show()
+    

464faf404069055f75f0432910bfc0138350040e
Andrey Sarantsev <50476640+asarantsev@users.noreply.github.com>
2024-12-13 18:39:01 -0800

    Update real-world-curves.py

--- real-world-curves.py
+++ real-world-curves.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 DF = pandas.read_excel('size-break-points.xlsx')
 values = numpy.array(DF.values[:, 2:-1].astype(float))
 xaxis = numpy.log(numpy.arange(1, 20, 1))
-for k in numpy.arange(0, 1188, 60):
+for k in numpy.arange(0, 1188, 240):
     data = numpy.log(values[k, ::-1]/sum(values[k]))
     plt.plot(xaxis, data, label = 'End of year '+str(1925+int(k/12)))
 plt.legend(loc = 'lower left')
